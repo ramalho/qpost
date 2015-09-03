ee5b4fffd3d162bcbb282b6150691adb8c0657be
Luciano Ramalho <luciano@ramalho.org>
2015-09-03 17:57:17 -0300

    init file example

---
+++ startup/init.py
@@ -0,0 +1,6 @@
+nuke.pluginAddPath( './_gizmos' )
+nuke.pluginAddPath('/Users/gui/.nuke/plugins/geometry/geometry/geometry')
+
+# LR
+import sys
+sys.path.append('/Users/gui/Documents/PythonScripts')
